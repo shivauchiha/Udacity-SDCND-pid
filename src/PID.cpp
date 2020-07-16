f9836832e57193ae85a3124a9cc5ef63ac193311
shyam <vasank1958@gmail.com>
2020-07-16 08:29:32 +0000

    first commit

---
+++ src/PID.cpp
@@ -0,0 +1,37 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  Kp=Kp_;
+  Ki=Ki_;
+  Kd=Kd_;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -((p_error*Kp)+(d_error*Kd)+(i_error*Ki));  // TODO: Add your total error calc here!
+}
